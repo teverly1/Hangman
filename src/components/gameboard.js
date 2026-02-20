add6fee5706baccfd1eaff3478cad1b0e9e0f57b
Tim Everly <everly@gmail.com>
2026-02-20 13:46:09 -0500

    Add Confetti Cannon

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -4,16 +4,16 @@ import Word from "./word";
 import GameOver from "./gameover";
 import DifficultySelector from "./difficultyselector";
 import { useState, useEffect } from "react";
-function Gameboard(props){
+function Gameboard(props) {
     const [word, setWord] = useState("eabcefg"),
-          [wordLength, setWordLength] = useState(7),
-          [loading, setLoading] = useState(false),
-          [uniqueLetters, setUniqueLetters] = useState(getUniqueLetters(word)),
-          [allGuesses, setAllGuesses] = useState({}),
-          [correctGuesses, setCorrectGuesses] = useState({}),
-          [missedCount, setMissedCount] = useState(0),
-          [gameOver, setGameOver] = useState(0),
-          failedCount = 5;
+        [wordLength, setWordLength] = useState(7),
+        [loading, setLoading] = useState(false),
+        [uniqueLetters, setUniqueLetters] = useState(getUniqueLetters(word)),
+        [allGuesses, setAllGuesses] = useState({}),
+        [correctGuesses, setCorrectGuesses] = useState({}),
+        [missedCount, setMissedCount] = useState(0),
+        [gameOver, setGameOver] = useState(0),
+        failedCount = 5;
 
     async function getAWord() {
         setLoading(true);
@@ -33,92 +33,86 @@ function Gameboard(props){
         }
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         restart(); // Automatically get a new word with the new length
-    },[wordLength])
+    }, [wordLength])
 
-    function onLetterGuessed(syntheticEvent){
-        let letter = syntheticEvent.nativeEvent.srcElement.innerText,
-            newAllGuesses = {...allGuesses},
+    function onLetterGuessed(letter) {
+        let newAllGuesses = { ...allGuesses },
             newCorrectGuesses,
             newMissedCount = 0,
-            isWinner=0;
-        
+            isWinner = 0;
+
         //update all guesses so we know which letters have been guessed
-        newAllGuesses[letter]=true;
+        newAllGuesses[letter] = true;
         setAllGuesses(newAllGuesses);
-        if(word.includes(letter))
-        {
+        if (word.includes(letter)) {
             //keep track of the guesses that are correct
-            newCorrectGuesses = {...correctGuesses};
-            newCorrectGuesses[letter]=true;
+            newCorrectGuesses = { ...correctGuesses };
+            newCorrectGuesses[letter] = true;
             setCorrectGuesses(newCorrectGuesses);
             //check if we have a winning guess
             isWinner = Object.keys(newCorrectGuesses).length === Object.keys(uniqueLetters).length;
-            if(isWinner)
-            {
+            if (isWinner) {
                 setGameOver(1);
             }
         }
-        else
-        {
-            newMissedCount=missedCount+1
+        else {
+            newMissedCount = missedCount + 1;
             setMissedCount(newMissedCount);
-            if(newMissedCount >= failedCount)
-            {
+            if (newMissedCount >= failedCount) {
                 setGameOver(2);
             }
         }
-        
     }
 
-    function reset(){
+    function reset() {
         setAllGuesses({});
         setCorrectGuesses({});
         setMissedCount(0);
         setGameOver(0);
     }
 
-    function restart(){
+    function restart() {
         reset();
         getAWord();
     }
 
-    function getUniqueLetters(word=""){
-        return word.split('').reduce((acc,curr)=> (acc[curr]='',acc),{})
+    function getUniqueLetters(word = "") {
+        return word.split('').reduce((acc, curr) => (acc[curr] = '', acc), {})
     }
 
-    const handleDifficultyChange = (newLength) => {
+    function handleDifficultyChange(newLength) {
         setWordLength(newLength);
-        // restart(); // Automatically get a new word with the new length
-    };
-
-    let buttons = [];
-    for(let x = 65; x < 91; x++) //Go from A(65)-Z(91) to create the buttons for the game
-    {
-        const letter = String.fromCharCode(x)
-        buttons.push(<LetterButton 
-            letter={letter}
-            key={x}
-            guessed={allGuesses[letter] || gameOver || loading}
-            handler={onLetterGuessed}
-        ></LetterButton>);
     }
+
+    // Generate buttons A-Z
+    const buttons = Array.from({ length: 26 }, (_, i) => {
+        const letter = String.fromCharCode(65 + i);
+        return (
+            <LetterButton
+                letter={letter}
+                key={letter}
+                guessed={!!allGuesses[letter] || !!gameOver || loading}
+                handler={() => onLetterGuessed(letter)} // Pass letter directly
+            />
+        );
+    });
 
     return <div className="gameboard">
-        <DifficultySelector 
-            onSelect={handleDifficultyChange} 
-            currentDifficulty={wordLength} 
+        <DifficultySelector
+            onSelect={handleDifficultyChange}
+            currentDifficulty={wordLength}
         />
-        {loading?<div><p className="loading-text">Loading </p><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>:
-        <Word word={word} guesses={allGuesses}></Word>}
+        {loading ? <div><p className="loading-text">Loading </p><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div> :
+            <Word word={word} guesses={allGuesses}></Word>}
         <div className="misses-wrapper">
             <Misses missedCount={missedCount}></Misses>
         </div>
         <div className="letters">
             {buttons}
         </div>
-        {gameOver?<GameOver onNewGame={restart} onReset={reset} gameOver={gameOver} word={word} missedCount={missedCount}></GameOver>:null}
+        {gameOver ? <GameOver onNewGame={restart} onReset={reset} gameOver={gameOver} word={word} missedCount={missedCount}></GameOver> : null}
     </div>
 }
 
--- src/components/gameover.js
+++ src/components/gameover.js
@@ -1,21 +1,68 @@
 import Misses from "./misses";
+import confetti from 'canvas-confetti';
+import { useEffect, useRef } from "react";
 
-function makeBouncyLetters(word){
+function makeBouncyLetters(word) {
     const wordSplit = word.split('');
-    return  wordSplit.map((letter,index) => {
+    return wordSplit.map((letter, index) => {
         return <span key={index} className="winner-animation">{letter}</span>
     })
 }
 
-function GameOver({onNewGame, onReset, gameOver=1, word="", missedCount=0}){
-//todo add button to show the word on a loss
-    let statusMessage = gameOver===1?makeBouncyLetters("You Win!"):"Game Over";
-    // let wordSplit = gameOver===1?word.split(''):'';
+function GameOver({ onNewGame, onReset, gameOver = 1, word = "", missedCount = 0 }) {
+    const animationFrameId = useRef(null),
+        winner = gameOver === 1;
+    let statusMessage = winner ? makeBouncyLetters("You Win!") : "Game Over";
+
+    useEffect(() => {
+        if (winner) {
+            showConfetti();
+        }
+        // The cleanup function
+        return stopAnnimation
+    }, [])
+
+    function showConfetti() {
+        let end = Date.now() + (15 * 1000);
+        const colors = ['#4A90E2', '#2ecc71', '#f1c40f'];
+
+        (function frame() {
+            const colors = ['#4A90E2', '#2ecc71', '#f1c40f'];
+
+            confetti({
+                particleCount: 2,
+                angle: 60,
+                spread: 55,
+                origin: { x: 0 },
+                colors: colors
+            });
+            confetti({
+                particleCount: 2,
+                angle: 120,
+                spread: 55,
+                origin: { x: 1 },
+                colors: colors
+            });
+
+            if (Date.now() < end) {
+                animationFrameId.current = requestAnimationFrame(frame);
+            }
+        }())
+    }
+
+    function stopAnnimation() {
+        // Cancel the animation frame request when the component unmounts
+        if (animationFrameId && animationFrameId.current) {
+            cancelAnimationFrame(animationFrameId.current);
+        }
+    }
+
+    //todo add button to show the word on a loss
     return <div className="modal">
         <div className="gameover">
             <div>
                 {
-                    gameOver===1?<div>{word}</div>:''
+                    winner ? <div>{word}</div> : ''
                 }
                 <p className="status">{statusMessage}</p>
                 <button onClick={onReset}>Restart?</button>
